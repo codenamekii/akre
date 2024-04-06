da002ace4898b21187b826f9fdaba540e9f7a06e
Dimas Yudistira <alex.yudistira44@gmail.com>
2024-04-06 23:58:04 +0700

    better error handling on API request

--- app/Models/GoogleSheet.php
+++ app/Models/GoogleSheet.php
@@ -14,27 +14,32 @@ class GoogleSheet extends Model
 
   public function fetchSheet($sheet, $range)
   {
-    $client = new GoogleClient();
-    $client->setAuthConfig('..\storage\app\uinsu-aipt-visualisasi-data-9b9dab81d2c0.json');
-    $client->addScope(Sheets::SPREADSHEETS_READONLY);
-    $sheets = new Sheets($client);
+    try {
+      $client = new GoogleClient();
+      $client->setAuthConfig('..\storage\app\uinsu-aipt-visualisasi-data-9b9dab81d2c0.json');
+      $client->addScope(Sheets::SPREADSHEETS_READONLY);
+      $sheets = new Sheets($client);
 
-    $spreadsheetId = env('SPREADSHEET_ID');
-    $locator = $sheet.'!'.$range;
+      $spreadsheetId = env('SPREADSHEET_ID');
+      $locator = $sheet.'!'.$range;
 
-    // $params = array('alt' => 'json');
+      // $params = array('alt' => 'json');
 
-    $response = $sheets->spreadsheets_values->get($spreadsheetId, $locator);
-    $values = $response->getValues();
+      $response = $sheets->spreadsheets_values->get($spreadsheetId, $locator);
+      $values = $response->getValues();
 
-    $keys = array_shift($values);
-    $result = [];
+      $keys = array_shift($values);
+      $result = [];
 
-    foreach ($values as $value) {
-      $result[] = array_combine($keys, $value);
+      foreach ($values as $value) {
+        $result[] = array_combine($keys, $value);
+      }
+
+      return json_encode($result);
+    } catch (\Throwable $th) {
+      return response($th->getMessage(), 500);
     }
-
-    return json_encode($result);
+    
   }
 
   /**
