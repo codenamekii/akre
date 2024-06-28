d88bd035c3f14158f5213ff63041a0d397127023
Dimas Yudistira <alex.yudistira44@gmail.com>
2024-06-28 21:38:53 +0700

    fix: visualisasi router accessible for public

--- routes/web.php
+++ routes/web.php
@@ -15,33 +15,31 @@ Route::middleware(['auth', 'security-header'])->group(function () {
     Route::get('/logout', [LoginController::class, 'deauthenticate']);
     Route::get('/', [LandingController::class, 'index'])->name('dashboard')->middleware('no-cache');
     Route::get('/daftar-dokumen', [DokumenController::class, 'getDokumen']);
+});
 
-    Route::prefix('visualisasi')->group(function () {
+Route::middleware(['security-header'])->prefix('visualisasi')->group(function () {
+    Route::view('/', 'visualisasi.index')->name('visualisasi.index');
     
-        Route::view('/', 'visualisasi.index')->name('visualisasi.index');
-        
-        Route::prefix('mahasiswa')->group(function () {
-            Route::get('{status}/{jenjang}', [VisualisasiController::class, 'show']);
+    Route::prefix('mahasiswa')->group(function () {
+        Route::get('{status}/{jenjang}', [VisualisasiController::class, 'show']);
 
-            Route::view('mahasiswa-tugas-akhir', 'visualisasi.mahasiswa.mahasiswa-tugas-akhir');
-            Route::view('mahasiswa-asing', 'visualisasi.mahasiswa.mahasiswa-asing');
-        });
-    
-        Route::prefix('sdm')->group(function () {
-            Route::prefix('dosen')->group(function () {
-                Route::view('per-homebase', 'visualisasi.sdm.dosen.dosen-perHomebase');
-                Route::view('per-jabatan', 'visualisasi.sdm.dosen.dosen-perJabatan');
-                Route::view('per-pendidikan', 'visualisasi.sdm.dosen.dosen-perPendidikan');
-                Route::view('per-sertifikasi', 'visualisasi.sdm.dosen.dosen-perSertifikasi');
-                Route::view('per-tidak-tetap', 'visualisasi.sdm.dosen.dosen-perTidakTetap');
-            });
-            
-            Route::view('tendik', 'visualisasi.sdm.tendik');
-        });
-    
-        Route::view('akreditasi', 'visualisasi.akreditasi');
+        Route::view('mahasiswa-tugas-akhir', 'visualisasi.mahasiswa.mahasiswa-tugas-akhir');
+        Route::view('mahasiswa-asing', 'visualisasi.mahasiswa.mahasiswa-asing');
     });
-    
+
+    Route::prefix('sdm')->group(function () {
+        Route::prefix('dosen')->group(function () {
+            Route::view('per-homebase', 'visualisasi.sdm.dosen.dosen-perHomebase');
+            Route::view('per-jabatan', 'visualisasi.sdm.dosen.dosen-perJabatan');
+            Route::view('per-pendidikan', 'visualisasi.sdm.dosen.dosen-perPendidikan');
+            Route::view('per-sertifikasi', 'visualisasi.sdm.dosen.dosen-perSertifikasi');
+            Route::view('per-tidak-tetap', 'visualisasi.sdm.dosen.dosen-perTidakTetap');
+        });
+        
+        Route::view('tendik', 'visualisasi.sdm.tendik');
+    });
+
+    Route::view('akreditasi', 'visualisasi.akreditasi');
 });
 
 Route::middleware(['auth', 'is-admin', 'security-header'])->group(function () {
